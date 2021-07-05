e6b57a9f136b6d46e227497d1f689c9ae7b221c3
arturo29ortiz <77816040+arturo29ortiz@users.noreply.github.com>
2021-07-05 02:38:05 -0500

    Initial commit

---
+++ client/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-console */
+/* eslint-disable no-alert */
+import './stylesheets/style.css';
+import './stylesheets/mystyles.css';
+// Inicializando scrip de materialize
+document.addEventListener('DOMContentLoaded', () => {
+  const sideNav = document.querySelectorAll('.sidenav');
+  // eslint-disable-next-line no-undef
+  M.Sidenav.init(sideNav);
+});
